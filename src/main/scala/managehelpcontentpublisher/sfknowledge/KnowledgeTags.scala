da3c3681a0f99885e2aeba312832510803dc26e7
David Pepper <david.pepper@guardian.co.uk>
2021-01-12 16:11:33 +0000

    Update KnowledgeTags case class

--- src/main/scala/managehelpcontentpublisher/sfknowledge/KnowledgeTags.scala
+++ src/main/scala/managehelpcontentpublisher/sfknowledge/KnowledgeTags.scala
@@ -5,13 +5,9 @@ object KnowledgeTags {
       Name: String
   )
   case class Records(
-      Id: String,
-      EntityId: String,
       Topic: Topic
   )
   case class RootInterface(
-      totalSize: Double,
-      done: Boolean,
       records: Seq[Records]
   )
 }

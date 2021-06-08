d3ec9e7d161c94a6be684307f31fbb6ef59e80fb
Kelvin Chappell <kelvin.chappell@theguardian.com>
2021-06-08 09:34:50 +0100

    Move test fixtures into resource files (#115)

--- src/test/scala/managehelpcontentpublisher/PathAndContentTestSuite.scala
+++ src/test/scala/managehelpcontentpublisher/PathAndContentTestSuite.scala
@@ -1,6 +1,5 @@
 package managehelpcontentpublisher
 
-import managehelpcontentpublisher.Config.config
 import utest._
 
 import java.net.URI
@@ -12,193 +11,16 @@ object PathAndContentTestSuite extends TestSuite {
 
   object Fixtures {
 
-    val article1: (String, String) = "how-can-i-redirect-my-delivery" ->
-      """{
-        |  "title": "How can I redirect my delivery?",
-        |  "body": [
-        |    {
-        |      "element": "p",
-        |      "content": [
-        |        {
-        |          "element": "text",
-        |          "content": "Our customer service team will ..."
-        |        }
-        |      ]
-        |    }
-        |  ],
-        |  "path": "how-can-i-redirect-my-delivery",
-        |  "topics": [
-        |    {
-        |      "path": "delivery",
-        |      "title": "Delivery"
-        |    }
-        |  ]
-        |}""".stripMargin
+    def resource(kind: String, id: String): (String, String) =
+      id -> Source.fromResource(s"${kind}s/$id.json").mkString
+    def article(id: String): (String, String) = resource("article", id)
+    def topic(id: String): (String, String) = resource("topic", id)
 
-    val article2: (String, String) = "can-i-read-your-papermagazines-online" ->
-      """{
-        |  "title": "Can I read your paper/magazines online?",
-        |  "body": [
-        |    {
-        |      "element": "p",
-        |      "content": [
-        |        {
-        |          "element": "text",
-        |          "content": "We do not"
-        |        }
-        |      ]
-        |    }
-        |  ],
-        |  "path": "can-i-read-your-papermagazines-online",
-        |  "topics": [
-        |    {
-        |      "path": "apps",
-        |      "title": "The Guardian apps"
-        |    }
-        |  ]
-        |}""".stripMargin
-
-    val deliveryTopic: (String, String) = "delivery" ->
-      """{
-        |  "path": "delivery",
-        |  "title": "Delivery",
-        |  "articles": [
-        |    {
-        |      "path": "can-i-read-your-papermagazines-online",
-        |      "title": "Can I read your paper/magazines online?"
-        |    },
-        |    {
-        |      "path": "how-can-i-redirect-my-delivery",
-        |      "title": "How can I redirect my delivery?"
-        |    },
-        |    {
-        |      "path": "id-like-to-make-a-complaint-about-an-advertisement",
-        |      "title": "I'd like to make a complaint about an advertisement"
-        |    },
-        |    {
-        |      "path": "im-unable-to-comment-and-need-help",
-        |      "title": "I'm unable to comment and need help"
-        |    }
-        |  ]
-        |}""".stripMargin
-
-    val appsTopic: (String, String) = "apps" ->
-      """{
-        |  "path": "apps",
-        |  "title": "The Guardian apps",
-        |  "articles": [
-        |    {
-        |      "path": "can-i-read-your-papermagazines-online",
-        |      "title": "Can I read your paper/magazines online?"
-        |    },
-        |    {
-        |      "path": "how-can-i-redirect-my-delivery",
-        |      "title": "How can I redirect my delivery?"
-        |    },
-        |    {
-        |      "path": "id-like-to-make-a-complaint-about-an-advertisement",
-        |      "title": "I'd like to make a complaint about an advertisement"
-        |    },
-        |    {
-        |      "path": "im-unable-to-comment-and-need-help",
-        |      "title": "I'm unable to comment and need help"
-        |    }
-        |  ]
-        |}""".stripMargin
-
-    val moreTopics: (String, String) = config.topic.moreTopics.path ->
-      """{
-      |  "path": "more-topics",
-      |  "title": "More Topics",
-      |  "topics": [
-      |    {
-      |      "path": "the-guardian-apps",
-      |      "title": "The Guardian apps",
-      |      "articles": [
-      |        {
-      |          "path": "a2",
-      |          "title": "Apple/Google subscriptions"
-      |        },
-      |        {
-      |           "path": "can-i-read-your-papermagazines-online",
-      |           "title": "Can I read your paper/magazines online?"
-      |        },
-      |        {
-      |          "path": "a3",
-      |          "title": "Personalising your apps"
-      |        },
-      |        {
-      |          "path": "a1",
-      |          "title": "Premium tier access"
-      |        }
-      |      ]
-      |    },
-      |    {
-      |      "path": "newsletters-and-emails",
-      |      "title": "Newsletters and emails",
-      |      "articles": [
-      |        {
-      |          "path": "n1",
-      |          "title": "I'm not receiving any emails from you but think I should be"
-      |        },
-      |        {
-      |          "path": "n2",
-      |          "title": "Manage your email preferences"
-      |        }
-      |      ]
-      |    },
-      |    {
-      |      "path": "events",
-      |      "title": "Events",
-      |      "articles": [
-      |        {
-      |          "path": "e1",
-      |          "title":
-      |            "I can no longer attend the live online event, can I have a refund?"
-      |        },
-      |        {
-      |          "path": "e2",
-      |          "title":
-      |            "I can’t find my original confirmation email, can you resend me the event link?"
-      |        },
-      |        {
-      |          "path": "e3",
-      |          "title":
-      |            "Once I have purchased a ticket, how will I attend the online event?"
-      |        },
-      |        {
-      |          "path": "e4",
-      |          "title": "I purchased a book with my ticket, when will I receive this?"
-      |        }
-      |      ]
-      |    },
-      |    {
-      |      "path": "gifting",
-      |      "title": "Gifting",
-      |      "articles": [
-      |        {
-      |          "path": "g1",
-      |          "title": "Gifting a Digital Subscription"
-      |        }
-      |      ]
-      |    },
-      |    {
-      |      "path": "archives",
-      |      "title": "Back issues and archives",
-      |      "articles": [
-      |        {
-      |          "path": "b1",
-      |          "title": "Finding articles from the past in digital format"
-      |        },
-      |        {
-      |          "path": "b2",
-      |          "title": "Old newspapers in physical format"
-      |        }
-      |      ]
-      |    }
-      |  ]
-      |}""".stripMargin
-
+    val article1: (String, String) = article("how-can-i-redirect-my-delivery")
+    val article2: (String, String) = article("can-i-read-your-papermagazines-online")
+    val deliveryTopic: (String, String) = topic("delivery")
+    val appsTopic: (String, String) = topic("apps")
+    val moreTopics: (String, String) = topic("more-topics")
     val sitemap: Set[URI] = Source.fromResource("sitemap.txt").getLines().map(new URI(_)).toSet
   }
 
